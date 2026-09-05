63d9fe08e089ceeb419fd04f92833c9785996abf
cwarje <cwarje@gmail.com>
2026-09-05 14:45:47 -0700

    New label removal

--- src/games/registry.ts
+++ src/games/registry.ts
@@ -905,7 +905,6 @@ export const GAME_REGISTRY: Record<GameType, GameDefinition> = {
     hasCardDealing: true,
     randomizeSeatOrder: true,
     production: true,
-    showNewBadge: true,
     hudTitleLines: ['10s'],
   },
 
@@ -952,7 +951,6 @@ export const GAME_REGISTRY: Record<GameType, GameDefinition> = {
     OptionsPanel: BackgammonOptions,
     fullBoard: true,
     production: true,
-    showNewBadge: true,
     hudTitleLines: ['Backgammon'],
   },
 
